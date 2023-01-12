8785cf7cd52d8ca06890714f6271c30a7c08eea1
LA PHUOC LOC <86074521+LaPhuocLoc@users.noreply.github.com>
2023-01-13 03:16:25 +0900

    update

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'store'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'admin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
